89bfafb2529b6e0da1a4c200c7d88e2e2777ffa8
repl.it user <replituser@example.com>
2020-10-06 10:57:45 +0000

    Added code to demonstrate how a valiable can be used as a counter

---
+++ basics/decisions/simple_decision/counter.py
@@ -0,0 +1,21 @@
+print ("Print enter the first number") #Asks for first number
+n1 = int(input())
+print ("Print enter the second number") #Asks for second number
+n2 = int(input())
+print ("Print enter the third number") #Asks for third number
+n3 = int(input())
+Odd = 0 #Calculates the number of odds
+Even = 0 #Calculates the number of evens
+if (n1 % 2 == 0): #Checks if number 1 is even or odd
+  Even = Even + 1
+else:
+  Odd = Odd +1
+if (n2 % 2 == 0): #Checks if number 2 is even or odd
+  Even = Even + 1
+else:
+  Odd = Odd +1
+if (n3 % 2 == 0): #Checks if number 3 is even or odd
+  Even = Even + 1
+else:
+  Odd = Odd +1
+print ("There were", Even ,"even numbers and", Odd ,"odd numbers")
