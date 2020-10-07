92d11a78bc0085e164084e595f85938510146a13
repl.it user <replituser@example.com>
2020-10-07 17:59:48 +0000

    Added code to demonstrate the use of a logical or operator

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,8 @@
+print ("What type of adventure should I have?")
+adventure_type = input()
+if (adventure_type == "short" or adventure_type == "scary"):
+  print ("\nEntering the dark forest!")
+elif (adventure_type == "safe" or adventure_type == "long"):
+  print ("\nTaking the safe route!")
+else:
+  print ("\nNot sure which route to take.")
