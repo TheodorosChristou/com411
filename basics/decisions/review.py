832b7cc462c197bc2013c606c020c529537b99ac
repl.it user <replituser@example.com>
2020-10-07 19:10:14 +0000

    Added code to review the use of a decision,nesting and logical operators

--- basics/decisions/review.py
+++ basics/decisions/review.py
@@ -1,20 +1,20 @@
-print ("What do I hear?") #Asks the user to indentify what Beeb heard
+print ("What do I hear?") #Asks the user to indentify what Beeb hears
 hear = input()
 if (hear == "muffled steps"):
-  print ("\nWhat do I see around me?")
+  print ("\nWhat do I see around me?") #Asks the user to indentify what Beeb sees
   see = input()
-  if ((see == "wolf") or (see == "giant spider")):
+  if ((see == "wolf") or (see == "giant spider")): #Determines if what Beeb saw is dangerous
     print ("What defensive protocol should I initiate? (shouting,flamethrower,scary face)")
     defensive_protocol = input()
-    if ((defensive_protocol == "shouting") or (defensive_protocol == "flamethrower")):
+    if ((defensive_protocol == "shouting") or (defensive_protocol == "flamethrower")): #Determines if the defensive protocol is effective
       print ("The creature got afraid and ran away,I can keep going!")
     else:
         print ("Defensive Protocol did not work! Initiating running!")
-  elif ((see == "squirrel" ) or (see == "rabbit")):
+  elif ((see == "squirrel" ) or (see == "rabbit")): #Determines if what Beeb saw is safe to approach
     print ("\nIts an adorable animal,what are some ways to safetly approach it? (two suggestions)")
     approach1 = input()
     approach2 = input()
-    if ((approach1 == "walk slowly") and (approach2 == "calm sounds")):
+    if ((approach1 == "walk slowly") and (approach2 == "calm sounds")): #Determines if Beeb's approach was succesful 
       print ("Approached the animal succesfully and had a fun time playing around! Time to keep going!")
     else:
       print ("Approach was not succesful,time to keep going!")
