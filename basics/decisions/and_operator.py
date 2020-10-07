6f60dd18f7e0e2acba86a00a8014bb0cf3352ae4
repl.it user <replituser@example.com>
2020-10-07 18:11:06 +0000

    Added code to demontrate the use of a logical and operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,8 @@
+print ("What did I hear?") #Asks the user to indentify what Beeb hears
+hear = input()
+print ("What did I see?") #Asks the user to indentify what Beeb sees
+see = input()
+if ((hear == "grrr") and (see == "two red eyes")): #Determines if there is a scary creature
+  print ("\nThere is a scary creature. I should get out of here!")
+else:
+  print ("\nI am a little scared but I will continue.")
