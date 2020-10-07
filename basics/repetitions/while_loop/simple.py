811ee8c6855aaa44e8e36cd470b243b28b0ea84d
repl.it user <replituser@example.com>
2020-10-07 19:25:08 +0000

    Added code to demonstrate the use of a simple look

--- basics/repetitions/while_loop/simple.py
+++ basics/repetitions/while_loop/simple.py
@@ -1,5 +1,5 @@
-print ("How many cables should I remove?")
+print ("How many cables should I remove?") #Asks the user for the amount of caples to remove
 cables =  int(input())
-while (cables > 0):
+while (cables > 0): #Creates a loop until all caples are removed
   print ("Removed cable.")
   cables = cables - 1
