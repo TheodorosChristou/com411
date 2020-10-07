d4a66625e47ccf4d13e78bdb1b28197b30296e4c
repl.it user <replituser@example.com>
2020-10-07 17:23:24 +0000

    Added code to demonstrate the use of a counter

--- basics/decisions/simple_decision/counter.py
+++ basics/decisions/simple_decision/counter.py
@@ -1,8 +1,8 @@
-print ("Print enter the first number") #Asks for first number
+print ("Enter the first number") #Asks for first number
 n1 = int(input())
-print ("Print enter the second number") #Asks for second number
+print ("Enter the second number") #Asks for second number
 n2 = int(input())
-print ("Print enter the third number") #Asks for third number
+print ("Enter the third number") #Asks for third number
 n3 = int(input())
 Odd = 0 #Calculates the number of odds
 Even = 0 #Calculates the number of evens
@@ -11,7 +11,7 @@ if (n1 % 2 == 0): #Checks if number 1 is even or odd
 else:
   Odd = Odd +1
 if (n2 % 2 == 0): #Checks if number 2 is even or odd
-  Even = Even + 1
+  Even = Even + 1 
 else:
   Odd = Odd +1
 if (n3 % 2 == 0): #Checks if number 3 is even or odd
