108310685a33a6ccd2cc1635175071cb2ae365f5
repl.it user <replituser@example.com>
2020-10-07 19:11:38 +0000

    Added code to demonstrate the use of an or operator

--- basics/decisions/or_operator.py
+++ basics/decisions/or_operator.py
@@ -1,8 +1,8 @@
-print ("What type of adventure should I have?")
+print ("What type of adventure should I have?") #Asks the user what type of adventure Beeb should have 
 adventure_type = input()
-if ((adventure_type == "short") or (adventure_type == "scary")):
+if ((adventure_type == "short") or (adventure_type == "scary")): #Determines if its short or scary
   print ("\nEntering the dark forest!")
-elif ((adventure_type == "safe") or (adventure_type == "long")):
+elif ((adventure_type == "safe") or (adventure_type == "long")): #Determines if its safe or long
   print ("\nTaking the safe route!")
 else:
   print ("\nNot sure which route to take.")
