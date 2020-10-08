aca6b5d94e2646b9db6e36a42a653bf269bedc64
TheodorosChr <5CHRIT06@solent.ac.uk>
2020-10-08 11:17:38 +0000

    "Added code to demonstrate the use of membership operators with a for loop."

--- basics/repetitions/for_loop/membership_operators.py
+++ basics/repetitions/for_loop/membership_operators.py
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 print ("What phrase do you see?") #Asks the user to input what they see
+=======
+print ("What phrase do you see?")
+>>>>>>> origin/main
 phrase = input()
 reverse = ""
 print ("\nReversing....\n")
 print ("The phrase is: ", end="")
+<<<<<<< HEAD
 for letter in phrase: #Begins the loop for each letter in the phrase given until it reverses it
+=======
+for letter in phrase:
+>>>>>>> origin/main
   reverse = letter + reverse
 print (reverse, end="")
