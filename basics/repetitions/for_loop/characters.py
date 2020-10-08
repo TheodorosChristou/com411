daf8795cbe57c4becf1791cc752d33552eec4a2e
TheodorosChr <5CHRIT06@solent.ac.uk>
2020-10-08 07:39:20 +0000

    Added code to demonstrate the use of a simple for loop

---
+++ basics/repetitions/for_loop/characters.py
@@ -0,0 +1,7 @@
+print ("What strange markings do you see?") #Asks the user to input the markings
+markings = input()
+print ("\nIdentifying.....")
+print ("")
+for count in range (0,len(markings),1): #Creates a loop for each character in the markings
+  print ("Index", count, ":",markings[count])
+print ("\nDone!")
