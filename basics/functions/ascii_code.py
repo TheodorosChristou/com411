9e75f22bcf4eeb74c7f104d0a7f324b88cf4db9e
repl.it user <replituser@example.com>
2020-10-13 10:17:31 +0000

    Added code to demonstrate the use of a built-in functions:len, ord

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,5 +1,8 @@
 print ("Program Started!")
 print ("Please enter a letter:") #Asks for a letter
 letter = input()
-print ("The ASCII code for",letter,"is: {}" .format(ord(letter))) #Transforms the letter into its ascii code and prints it
+if (len(letter) == 1): #Checks if the user put the correct input
+  print ("The ASCII code for",letter,"is: {}" .format(ord(letter))) #Transforms the letter into its ascii code and prints it
+else:
+  print ("Invalid input!")
 print ("Program Ended!")
