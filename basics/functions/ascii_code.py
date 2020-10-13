d927517b721e313d4260ef3c3e40ccb036a68e90
repl.it user <replituser@example.com>
2020-10-13 10:15:30 +0000

    Added code to demonstrate the functions

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,5 @@
+print ("Program Started!")
+print ("Please enter a letter:") #Asks for a letter
+letter = input()
+print ("The ASCII code for",letter,"is: {}" .format(ord(letter))) #Transforms the letter into its ascii code and prints it
+print ("Program Ended!")
