828f048b79862d398c285d8b63683642992d15a6
repl.it user <replituser@example.com>
2020-10-20 14:52:16 +0000

    Added code to demonstrate how to populate a list

--- data/lists/populate_list.py
+++ data/lists/populate_list.py
@@ -3,25 +3,24 @@ def directions(): #Creates a function that creates the first list and returns it
   return directions
 
 def menu(): #Creates a function that creates a loop that takes in the user's choices
-  print ("Please select a direction: ")
   direc = directions()
-  User_res = []
-  print ("Please select a direction")
-  for count in range(0 , 5, 1):
-    print("")
-    for count in range(0 , 4, 1):
-      print ("{0}:{1}" .format(count, direc[count]))
-    
-    User_r = int(input())
-    User_res.append(direc[User_r])
+ 
+  print("")
+  for count in range (0, 4, 1):
+    print ("{0}:{1}" .format(count, direc[count]))
+  User_r = int(input())
   print ("")
   
-  return User_res
+  return direc[User_r]
 
 def run(): #Creates a function that calls the list and prints in out
-  print ("Working out escape route...")
-  route = menu()
-  print ("Escape route {}" .format(route))
+  print ("Working out escape route...\n")
+  print ("Please select a direction:")
+  route = []
+  for count in range (0 , 5, 1):
+    droute = menu()
+    route.append(droute)
+  print ("Escape route: {}" .format(route))
 
 run()
 
