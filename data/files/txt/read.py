3e17ec6e6a99b0cfc85f18485c1762411afc2b7a
repl.it user <replituser@example.com>
2020-11-03 13:05:11 +0000

    Added code to demonstrate how to read a text file

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,14 +1,13 @@
 def search(filename):
   print ("Searching...")
   with open(filename) as file:
-    for line in file:
-      print(f"looked in {line}")
-  print ("Done")
+    lines  = file.read().split('\n')
+    for line in lines:
+      print(f"looked in {line}.")
+  print ("...Done!")
 
 def run():
-  location = search("data/files/txt/locations.txt")
-  search2 = search(location)
-  print(search2)
+ search("data/files/txt/locations.txt")
 
 run()
 
