c8fbee8beb729c0780a0339267b21a4af8fb5334
repl.it user <replituser@example.com>
2020-11-24 11:12:10 +0000

    Added code to visualise a sine wave

---
+++ visual/animation/sine.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+import numpy as np 
+
+fig, ax = plt.subplots()
+
+def animate(frame):
+ ax.set_xlim(0, 720)
+ ax.set_ylim(-1, 1)
+ x_values = np.arange(0, frame)
+ y_values = np.sin((np.pi / 180) * x_values)
+ ax.plot(x_values, y_values, 'r--')
+
+def run():
+  simple_animation = animation.FuncAnimation(fig,animate, frames = 720, interval = 100 )
+  plt.show()
+
+run()
+
+
