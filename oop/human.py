eb938cd1d4afdb156128c0c33e3ba197f471cefe
repl.it user <replituser@example.com>
2020-12-02 13:29:52 +0000

    Added code to create a basic human class

--- oop/human.py
+++ oop/human.py
@@ -7,6 +7,14 @@ class Human:
   def display(self):
     print(f"I am {self.name}")
 
+  def __repr__(self):
+   return f'human(name={self.name}, age={self.age}. energy ={self.energy})'
+
+  def __str__(self):
+   return f'My name is {self.name} and I am {self.age} years old.'
+
 if (__name__ == "__main__"):
   human = Human()
   human.display()
+  print(human)
+  print(repr(human))
