f9c668611d63dd0ef8f859ba03948984486ec825
repl.it user <replituser@example.com>
2020-12-19 15:54:02 +0000

    Added code to create a jetpack class

---
+++ oop/jetback.py
@@ -0,0 +1,23 @@
+from tech import Tech
+
+class Jetpack(Tech):
+
+  def __init__(self):
+    super().__init__()
+
+  def __repr__(self):
+    return "jetpack()"
+
+  def activate(self):
+    print ("Activating Jetpack")
+
+  def deactivate(self):
+    print ("Deactivating Jetpack")
+
+  def fly(self, speed):
+    print (f"Flying at speed of {speed}")
+
+if __name__ == "__main__":
+  jetpack = Jetpack()
+  print(repr(jetpack))
+
